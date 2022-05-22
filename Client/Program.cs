cc3d0ebf61d39860cf9f7bb71628e3e2a8e03426
Brett Gadd <brettgadd@gmail.com>
2022-05-22 14:54:12 -0700

    Adding tailwind

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using GaddLifeApp.Client;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient
+{
+  BaseAddress = new Uri(builder.Configuration["API_Prefix"] ??
+  builder.HostEnvironment.BaseAddress)
+});
+builder.Services.AddBlazoredLocalStorage();
+
 
 await builder.Build().RunAsync();
