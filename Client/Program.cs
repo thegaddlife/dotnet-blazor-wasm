81f8494bd21fe0f4e47ae8e78aecb451db82e7f4
Brett Gadd <brettgadd@gmail.com>
2022-05-23 21:07:32 -0700

    simplify project for now

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,11 +7,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient
-{
-  BaseAddress = new Uri(builder.Configuration["API_Prefix"] ??
-  builder.HostEnvironment.BaseAddress)
-});
 builder.Services.AddBlazoredLocalStorage();
 
 
